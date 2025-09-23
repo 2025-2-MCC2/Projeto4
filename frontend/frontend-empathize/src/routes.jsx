65f1c67f48f3ab50803fd32ffff21fbde1bde465
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2025-09-23 17:15:21 -0300

    Criei o build para deploy

--- frontend/frontend-empathize/src/routes.jsx
+++ frontend/frontend-empathize/src/routes.jsx
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
     {
         path: "/register",
         element: <Form />
+    },
+    {
+        path: "*",
+        element: <DashboardStudent />
     }
 ])
 
