25bc5d3224ab830ae0351a60ca62288b312495b0
leticia19silva <leticia19contato@gmail.com>
2025-10-20 22:41:30 -0300

    feat: Alterei o layout usando Flexbox e Grid, adicionei animação dos cards e painel de mensagens

--- frontend/frontend-empathize/src/components/MainContainer/BoxComponent/index.jsx
+++ frontend/frontend-empathize/src/components/MainContainer/BoxComponent/index.jsx
@@ -2,14 +2,20 @@ import styles from "./style.module.css";
 
 export default function BoxComponent(props) {
     return (
-        <>
-            <div className="leftSide">
-                <span className={styles.numbers}>{props.pontuation} </span>
+        <div className={styles.boxContent}>
+            
+            <div className={styles.textAndTitleWrapper}> 
+                
+                <span className={styles.numbers}>{props.pontuation}</span>
+                
                 <p className={styles.title}>{props.title}</p>
+                
             </div>
-            <div className={styles.rigthSide}>
-                <img src={props.icon} alt="" className={styles.icons}/>
+            
+            <div className={styles.iconWrapper}> 
+                <img src={props.icon} alt={props.title} className={styles.icons}/>
             </div>
-        </>
+            
+        </div>
     );
 }
--- frontend/frontend-empathize/src/components/MainContainer/index.jsx
+++ frontend/frontend-empathize/src/components/MainContainer/index.jsx
@@ -1,4 +1,5 @@
 import BoxComponent from "./BoxComponent";
+import MessagePanel from "./MessagePanel";
 import styles from "./style.module.css";
 import iconRanking from "../../assets/estrelas-do-ranking.svg";
 import iconFood from "../../assets/tigela-de-macarrao-com-pauzinhos.svg";
@@ -6,8 +7,10 @@ import iconGoals from "../../assets/seta-de-alvo.svg";
 
 export default function MainContainer() {
     return (
-        <>
-            <div className={styles.container}>
+        <div className={styles.container}> 
+            
+            <div className={styles.cardsWrapper}> 
+                
                 <div className={styles.box}>
                     <BoxComponent pontuation="0" title="Pontuação" icon={iconRanking}/>
                 </div>
@@ -15,9 +18,12 @@ export default function MainContainer() {
                     <BoxComponent pontuation="3kg" title="Arrecadação" icon={iconFood}/>
                 </div>
                 <div className={styles.box}>
-                    <BoxComponent pontuation="2" title="Metas atingidas" icon={iconGoals}/>    
-                </div>    
-            </div>  
-        </>
+                    <BoxComponent pontuation="2" title="Metas atingidas" icon={iconGoals}/>     
+                </div> 
+            </div>
+
+            <MessagePanel className={styles.messagePanel} />
+
+        </div>     
     );
 }
