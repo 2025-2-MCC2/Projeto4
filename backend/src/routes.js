4144641048ebdc3096653205061f43049d541584
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2025-10-21 16:34:20 -0300

    fiz o mentor controller e alterei algumas coisas no user controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,26 +1,39 @@
 import express from "express";
 import usersController from "./controllers/usersController.js";
 import groupsController from "./controllers/groupsController.js";
+import mentorController from "./controllers/mentorController.js";
 
 const router = express.Router();
 
 // Rotas GET
 router.get("/allUsers", usersController.allUsers);
 router.get("/user/:id", usersController.userByID);
+router.get("/userRA/:RA", usersController.userByRA);
+
 router.get("/allGroups", groupsController.allGroups);
 router.get("/group/:id", groupsController.groupByID);
 
 // Rotas POST
 router.post("/createUser", usersController.createUser);
 router.post("/signin", usersController.login);
+
 router.post("/createGroup", groupsController.createGroup);
 
+router.post("/createMentor", mentorController.createMentor);
+router.post("/loginMentor", mentorController.loginMentor);
+
 // Rotas PUT
-router.put("/updateUser", usersController.updateUser);
+router.put("/updateUser/:id", usersController.updateUser);
+
+router.put("updateMentor/:id", mentorController.updateMentor);
+
 router.put("/updateGroup/:idGroup", groupsController.updateGroup);
 
 // Rotas DELETE
 router.delete("/deleteUser/:id", usersController.deleteUser);
+
+router.delete("/deleteMentor/:id", mentorController.deleteMentor);
+
 router.delete("/deleteGroup/:id", groupsController.deleteGroup);
 
 export default router;
