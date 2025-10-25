54fbca0fe10b6eed0e1d07173f3ed685bc1e575f
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2025-10-25 14:44:23 -0300

    Adicionei o cors, também mudei o faticon do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import routes from "./routes.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}));
 app.use(routes);
+app.use(cors({
+    origin: "https://empathize-system.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor está rodando na porta http://localhost:${PORT}`));
